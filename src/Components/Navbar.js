6ddcc4023894aea70062d426b6fee1ee396639d9
Mohd Uvaish <81020580+uvaishnasir@users.noreply.github.com>
2023-09-08 18:14:32 +0530

    change navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,7 +12,6 @@ const NavBar = () =>{
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
                         <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
                         <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
                         <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
@@ -26,6 +25,5 @@ const NavBar = () =>{
             </nav>
         </div>
     )
-    
 }
 export default NavBar;
